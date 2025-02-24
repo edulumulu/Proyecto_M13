175f306d253dcd3b98de9d3fd74a387055cefbca
edulumulu <eduardolucasmunozdelucas@gmail.com>
2025-02-24 19:24:48 +0100

    Cambio para trabaajr en mi rama

--- app/src/main/java/com/example/proyecto_m13/Ficha_cliente.java
+++ app/src/main/java/com/example/proyecto_m13/Ficha_cliente.java
@@ -22,5 +22,6 @@ public class Ficha_cliente extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        //SDLJDSLKDSLKDJSLK
     }
 }
