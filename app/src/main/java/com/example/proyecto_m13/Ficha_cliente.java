27a897b471b713a41bcf0cf2e8114f2c90986862
Carlos Herrera Rodríguez <carlosherrerarodriguez10@gmail.com>
2025-02-25 12:12:04 +0000

    Clase GestionBBDD (credenciales y buscar)

--- app/src/main/java/com/example/proyecto_m13/Ficha_cliente.java
+++ app/src/main/java/com/example/proyecto_m13/Ficha_cliente.java
@@ -2,12 +2,17 @@ package com.example.proyecto_m13;
 
 import android.content.pm.ActivityInfo;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
+import java.util.ArrayList;
 
 public class Ficha_cliente extends AppCompatActivity {
 
@@ -16,10 +21,27 @@ public class Ficha_cliente extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_ficha_cliente);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        /*Código que obtiene un arraylist de clientes, y obtiene los datos de cada cliente
+        y los mete en un arrayList de strings para mostrarlo en el adaptador
+        GestionBBDD gestionBBDD = new GestionBBDD();
+        gestionBBDD.listarClientes(this, new GestionBBDD.ClienteCallback() {
+            @Override
+            public void onClientesListados(ArrayList<Cliente> listaClientes) {
+                if (listaClientes != null && !listaClientes.isEmpty()) {
+                    // Aquí puedes actualizar la UI con los clientes
+                    ArrayList<String> listaClientesAdaptador = new ArrayList<>();
+                    for (Cliente cliente : listaClientes) {
+                        listaClientesAdaptador.add("Usuario: " + cliente.getUsuario() + "\nContraseña: " + cliente.getContrasena());
+                    }
+
+                    ArrayAdapter<String> adapter = new ArrayAdapter<>(Ficha_cliente.this, android.R.layout.simple_list_item_1, listaClientesAdaptador);
+                    ListView viewCliente = findViewById(R.id.viewCliente);
+                    viewCliente.setAdapter(adapter);
+                } else {
+                    Toast.makeText(Ficha_cliente.this, "No se encontraron clientes", Toast.LENGTH_LONG).show();
+                }
+            }
+        });*/
     }
 }
--- app/src/main/java/com/example/proyecto_m13/MainActivity.java
+++ app/src/main/java/com/example/proyecto_m13/MainActivity.java
@@ -1,18 +1,13 @@
 package com.example.proyecto_m13;
 
 import android.content.Intent;
-import android.content.pm.ActivityInfo;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
-
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -47,12 +42,12 @@ public class MainActivity extends AppCompatActivity {
             Toast.makeText(this, "No puedes dejar ningún campo vacío", Toast.LENGTH_LONG).show();
         }else {
             /* si rellenan los datos lanzariamos un método que comprueba las credenciales con la BBDD
-            gestion_BBDD.comprobarCredenciales();
+            gestionBBDD.comprobarCredenciales();
             En este caso abriremos la siguiente actividad para empezar a hacer pruebas
             Y le paso el usuario por si quieres mostrar el nombre arriba a la izquierda*/
             Intent intent = new Intent(MainActivity.this,Ficha_cliente.class);
-            startActivity(intent);
             intent.putExtra("usuario", usuario);
+            startActivity(intent);
         }
     }
 }
