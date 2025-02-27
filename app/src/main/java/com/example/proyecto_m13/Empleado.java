eb36b5f83d12105b3f21d63f75a13e498dc8257f
Bibiana <bibiana.martin@gmail.com>
2025-02-27 09:37:13 +0100

    Añadir modelo Empleado

---
+++ app/src/main/java/com/example/proyecto_m13/Empleado.java
@@ -0,0 +1,103 @@
+package com.example.proyecto_m13;
+
+public class Empleado {
+    private Integer idEmpleado;
+    private String nif;
+    private String nombre;
+    private String apellidos;
+    private String usuario;
+    private String contrasena;
+    private String email;
+    private String cargo;
+
+    public Empleado(Integer idEmpleado) {
+        this.idEmpleado = idEmpleado;
+    }
+
+    public Empleado(Integer idEmpleado, String nif, String nombre, String apellidos, String usuario, String contrasena, String email, String cargo) {
+        this.idEmpleado = idEmpleado;
+        this.nif = nif;
+        this.nombre = nombre;
+        this.apellidos = apellidos;
+        this.usuario = usuario;
+        this.contrasena = contrasena;
+        this.email = email;
+        this.cargo = cargo;
+    }
+
+    public Empleado(String nif, String nombre, String apellidos, String usuario, String contrasena, String email, String cargo) {
+        this.nif = nif;
+        this.nombre = nombre;
+        this.apellidos = apellidos;
+        this.usuario = usuario;
+        this.contrasena = contrasena;
+        this.email = email;
+        this.cargo = cargo;
+    }
+
+
+    public Integer getIdEmpleado() {
+        return idEmpleado;
+    }
+
+    public void setIdEmpleado(Integer idEmpleado) {
+        this.idEmpleado = idEmpleado;
+    }
+
+    public String getNif() {
+        return nif;
+    }
+
+    public void setNif(String nif) {
+        this.nif = nif;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+
+    public String getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(String usuario) {
+        this.usuario = usuario;
+    }
+
+    public String getContrasena() {
+        return contrasena;
+    }
+
+    public void setContrasena(String contrasena) {
+        this.contrasena = contrasena;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getCargo() {
+        return cargo;
+    }
+
+    public void setCargo(String cargo) {
+        this.cargo = cargo;
+    }
+
+}
