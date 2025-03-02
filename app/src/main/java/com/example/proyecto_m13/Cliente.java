658edba3f0587287c646a67a5067d75faac22a2f
Bibiana <bibiana.martin@gmail.com>
2025-03-02 18:02:24 +0100

    GestionBBDD metodos insert y update

--- app/src/main/java/com/example/proyecto_m13/Cliente.java
+++ app/src/main/java/com/example/proyecto_m13/Cliente.java
@@ -1,6 +1,5 @@
 package com.example.proyecto_m13;
 
-import java.time.LocalDate;
 import java.util.Calendar;
 import java.util.Date;
 
@@ -44,7 +43,7 @@ public class Cliente {
     }
     //constructor sin id
 
-    public Cliente(String name, String surname, String dni, Date date_born, int tlf, String email, String tutor, String street, int cp, String ciudad) {
+    public Cliente(String name, String surname, String dni, Date date_born, int tlf, String email, String tutor, boolean b, Date parse, String street, boolean b1, String s, int cp, String ciudad) {
         this.name = name;
         this.surname = surname;
         this.dni = dni;
--- app/src/main/java/com/example/proyecto_m13/MainActivity.java
+++ app/src/main/java/com/example/proyecto_m13/MainActivity.java
@@ -9,6 +9,9 @@ import android.widget.EditText;
 import android.widget.Toast;
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 
 
@@ -33,12 +36,16 @@ public class MainActivity extends AppCompatActivity {
                 @Override
                 public void onClick(View v) {
 
-                    iniciarSesion();
+                    try {
+                        iniciarSesion();
+                    } catch (ParseException e) {
+                        throw new RuntimeException(e);
+                    }
                 }
             });
     }
 
-    private void iniciarSesion(){
+    private void iniciarSesion() throws ParseException {
         final String usuario = etUsuario.getText().toString().trim();
         final String contrasena = etContrasena.getText().toString().trim();
 
@@ -48,6 +55,18 @@ public class MainActivity extends AppCompatActivity {
             //gestionBBDD.listarClientes(this,this);
             //gestionBBDD.eliminarCliente(this, 2);
             //gestionBBDD.comprobarCredenciales(this, usuario, contrasena);
+            SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy");
+
+            //lo inserto con id 1 aunque internamente va a tener otro id en la base de datos
+          /* gestionBBDD.insertarCliente(this, new Cliente(1,"Carlos", "Herrera",
+                   "520546567L", dateFormat.parse("06/02/2019"),
+                   123456789, "paco@gmail", "Antonio Lucas", false, null,
+                   null, false, "Valmo 27", 28047, "Barcelona")); y
+
+           /*gestionBBDD.modificarCliente(this,new Cliente(2, "Bibiana", "Martin",
+                    "520546668J", dateFormat.parse("03/04/1987"), 123456789, "edu@gmail",
+                    "Paco", true, dateFormat.parse("10/05/2019"),
+                 "gafas", false, "Illescas 27", 28047, "Madird"));*/
         }
     }
 }
